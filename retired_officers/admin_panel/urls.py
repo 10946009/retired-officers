28ce1a7b9645f9448c1c8800ef80dca90c1b29cd
lalalalaluk <lalalalaluk@gmail.com>
2024-03-03 19:12:59 +0000

    ✨feature: activity crud done

--- retired_officers/admin_panel/urls.py
+++ retired_officers/admin_panel/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from admin_panel.views.activity_manage import activity_manage
+from admin_panel.views.activity_create import activity_create
+from admin_panel.views.activity_update import activity_update
+from admin_panel.views.activity_delete import activity_delete
 
 urlpatterns = [
     path('activity_manage/', activity_manage, name='activity_manage'), 
+    path('activity_create/', activity_create, name='activity_create'), 
+    path('activity_update/<int:activity_id>', activity_update, name='activity_update'), 
+    path('activity_delete/<int:activity_id>', activity_delete, name='activity_delete'), 
 ]
--- retired_officers/admin_panel/views/activity_manage.py
+++ retired_officers/admin_panel/views/activity_manage.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from myapp.models import Activity
 
 def activity_manage(request):
-    return render(request, 'activity_manage.html')
+    # get all activties
+    activities = Activity.objects.all()
+
+
+    return render(request, 'activity_manage.html', {'activities': activities})
+
